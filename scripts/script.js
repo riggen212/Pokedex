b553fcc06a0439f8dca4099748f722f3f9061a84
riggen212 <bastiriggen212@gmail.com>
2026-06-07 19:53:44 +0200

    font-family added dialog for detail information added dialog styling implemented display creature detail-information in dialog added switching between creatures has been implemented in the dialog element loading data from the api has been improved

--- scripts/script.js
+++ scripts/script.js
@@ -1,51 +1,142 @@
-const MAIN_DATA_URL = 'https://pokeapi.co/api/v2/pokemon-form/';
-const MAIN_CREATURE_DATA = `api-cache-v1`;
 let CREATURE_ID = 1;
+let loadDataCount = 20;
+let currentSelectedCreature = "";
 const creatureCach = {};
 
-async function loadCreatureData(id) {
-    const cacheKey = id;
-    // 1. Prüfen, ob die Daten im lokalen In-Memory-Cache liegen
-    if (creatureCach[cacheKey]) {
-        console.log("Daten aus dem In-Memory-Cache geladen!");
-        return creatureCach[cacheKey];
+function init() {
+    for (let i = CREATURE_ID; i <= loadDataCount; i++) {
+        loadDataFromApi(i);
     }
-    // Prüfen, ob die Daten im LocalStorage des Browsers liegen
-    const cachedData = localStorage.getItem(cacheKey);
-    if (cachedData) {
-        // Wieder in ein Objekt umwandeln
-        const parsedData = JSON.parse(cachedData);
-        // Für schnellen Zugriff direkt im Speicher ablegen
-        creatureCach[cacheKey] = parsedData;
-        return parsedData;
-    }
-    // 3. Wenn nichts vorhanden, Daten von der PokéAPI abrufen
+    CREATURE_ID = loadDataCount + 1;
+}
+
+// load creature data from api
+async function loadDataFromApi(id) {
     try {
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${cacheKey}`);
-        if (!response.ok) throw new Error("Pokémon nicht gefunden");
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
         const data = await response.json();
-        // 4. Daten im In-Memory-Cache speichern
-        creatureCach[cacheKey] = data;
-        // 5. Daten im LocalStorage speichern (als String, daher JSON.stringify)
-        localStorage.setItem(cacheKey, JSON.stringify(data));
-        return data;
+        saveDataInMemory(id, data);
+        document.getElementById('cardContainer').innerHTML += renderCreatureCard(id, creatureCach[id]);
+        loadCreatureClassData(id, `creatureClass${id}`);
+
     } catch (error) {
         console.error("Fehler:", error);
     };
 }
 
-function init() {
-    for (let i = CREATURE_ID; i <= 24; i++) {
-        loadCreatureData(i);
+function saveDataInMemory(id, data) {
+    creatureCach[id] = data;
+}
 
-        // create creatureCards
-        let creatureData = JSON.parse(getCreatureDataFromLocalStorage(i));
-        document.getElementById('cardContainer').innerHTML += renderCreatureCards(i, creatureData);
-        CREATURE_ID++;
+function loadCreatureClassData(id, containerRef) {
+    const displayClassRef = document.getElementById(containerRef);
+
+    for (let i = 0; i < creatureCach[id].types.length; i++) {
+        displayClassRef.innerHTML += renderCreatureClass(creatureCach[id].types[i].type.name);
     };
 }
 
+function loadCreatureData(id) {
+    const cacheKey = id;
+    // check, is data in memory storage?
+    if (creatureCach[cacheKey]) {
+        return true;
+    } else {
+        return false
+    }
+}
 
-function getCreatureDataFromLocalStorage(id) {
-    return localStorage.getItem(id);
+function openDialog(id) {
+    const creatureId = id.match(/\d+/g);
+    const dialogRef = document.getElementById(`detailCard`);
+    dialogRef.innerHTML += renderDetailCard(creatureCach[creatureId]);
+    loadCreatureClassData(creatureId, `detailCardClass`);
+    loadMainData(getCreatureIdFromDialog());
+    currentSelectedCreature = creatureId;
+    dialogRef.showModal();
+}
+
+function closeDialog() {
+    const dialogRef = document.getElementById('detailCard');
+    resetDialog(dialogRef)
+    dialogRef.close();
+}
+// render dialog-detail informations
+function setMainData() {
+    resetDetailData();
+    loadMainData(getCreatureIdFromDialog());
+}
+
+function loadMainData(id) {
+    const height = creatureCach[id].height / 10;
+    const weight = creatureCach[id].weight / 10;
+    const baseExperience = creatureCach[id].base_experience;
+
+    let displayDetailRef = document.getElementById(`detailInformation`);
+    displayDetailRef.innerHTML += renderDetailMainInformation(height, weight, baseExperience);
+    loadAbilities(id);
+}
+
+function loadAbilities(id) {
+    for (let i = 0; i < creatureCach[id].abilities.length; i++) {
+        if (i == creatureCach[id].abilities.length - 1) {
+            document.getElementById(`abilities`).innerHTML += creatureCach[id].abilities[i].ability.name
+        } else {
+            document.getElementById(`abilities`).innerHTML += creatureCach[id].abilities[i].ability.name + `, `;
+        }
+    };
+}
+
+function setStatsData() {
+    resetDetailData();
+    loadStatsData(getCreatureIdFromDialog());
+}
+
+function loadStatsData(id) {
+    const hp = creatureCach[id].stats[0].base_stat;
+    const attack = creatureCach[id].stats[1].base_stat;
+    const defense = creatureCach[id].stats[2].base_stat;
+    const specialAttack = creatureCach[id].stats[3].base_stat;
+    const specialDefense = creatureCach[id].stats[4].base_stat;
+    const speed = creatureCach[id].stats[5].base_stat;
+
+    let displayDetailRef = document.getElementById(`detailInformation`);
+    displayDetailRef.innerHTML += renderDetailStatsInformation(hp, attack, defense, specialAttack, specialDefense, speed);
+}
+
+function resetDetailData() {
+    document.getElementById(`detailInformation`).children[0].remove();
+}
+
+// change data in dialog
+function previousCreature() {
+    if (currentSelectedCreature > 1) {
+        currentSelectedCreature--;
+        const dialogRef = document.getElementById(`detailCard`);
+        resetDialog(dialogRef);
+        dialogRef.innerHTML += renderDetailCard(creatureCach[currentSelectedCreature]);
+        loadCreatureClassData(currentSelectedCreature, `detailCardClass`);
+        loadMainData(getCreatureIdFromDialog());
+    };
+}
+
+function nextCreature() {
+    if (currentSelectedCreature < loadDataCount) {
+        currentSelectedCreature++;
+        const dialogRef = document.getElementById(`detailCard`);
+        resetDialog(dialogRef);
+        dialogRef.innerHTML += renderDetailCard(creatureCach[currentSelectedCreature]);
+        loadCreatureClassData(currentSelectedCreature, `detailCardClass`);
+        loadMainData(getCreatureIdFromDialog());
+    };
+}
+
+function resetDialog(parentRef) {
+    const cardRef = parentRef.children[0];
+    cardRef.remove();
+}
+
+// allgemeine funktionen
+function getCreatureIdFromDialog() {
+    return document.getElementById(`detailCard`).children[0].children[0].children[1].textContent;
 }
--- scripts/template.js
+++ scripts/template.js
@@ -1,12 +1,102 @@
-function renderCreatureCards(id, data) {       
-    return `<article class="creatureCard ${data.types[0].type.name}" id='creature${id}'>
+function renderCreatureCard(id, data) {          
+    return `<article class="creatureCard ${data.types[0].type.name}" id='creature${id}' onclick="openDialog(this.id)">
                 <header>
                     <h2>${data.name.charAt(0).toUpperCase() + data.name.slice(1)}</h2> <span>#${id}</span>
                 </header>
                 <img class="creatureCardImage" src="${data.sprites.other.dream_world.front_default}" alt="">
-                <aside>
-                    <span>${data.types[0].type.name.charAt(0).toUpperCase() + data.types[0].type.name.slice(1)}</span>
+                <aside id='creatureClass${id}'>
                 </aside>
             </article
+    `;
+}
+
+function renderCreatureClass(className) {
+    return `<span>${className.charAt(0).toUpperCase() + className.slice(1)}</span>
+    `;
+}
+
+function renderDetailCard(data) { 
+    return `<div class="detailCard ${data.types[0].type.name}" data-id="overlay-pokemon-name">
+            <header>
+                <h2 id="detailCardName">${data.name.charAt(0).toUpperCase() + data.name.slice(1)}</h2>
+                <span id="detailCardId">${data.id}</span>
+                <button data-id="close-dialog-button" onclick="closeDialog()">close</button>
+            </header>
+            <aside>
+                <div id="detailCardClass"></div>
+                <img data-id="card-image" id="detailCardImage" src="" alt="">
+            </aside>
+            <section>
+                <nav>
+                    <ul>
+                        <li onclick="setMainData()">main</li>
+                        <li onclick="setStatsData()">stats</li>
+                        <li>evo chain</li>
+                    </ul>
+                </nav>
+                <div id="detailInformation">
+                </div>
+            </section>
+            <div>
+                <button data-id="prev-button" onclick="previousCreature()">
+                    previous
+                </button>
+                <button data-id="next-button" onclick="nextCreature()">
+                    next
+                </button>
+            </div>
+        </div>
+    `;
+}
+
+function renderDetailMainInformation(height, weight, baseExperience) {
+    return `<table>
+            <tr>
+                <th>Height:</th>
+                <td>${height} m</td>
+            </tr>
+            <tr>
+                <th>Weight:</th>
+                <td>${weight} kg</td>
+            </tr>
+            <tr>
+                <th>Base Experience:</th>
+                <td>${baseExperience}</td>
+            </tr>
+            <tr>
+                <th>abilities:</th>
+                <td id="abilities"></td>
+            </tr>
+        </table>
+    `;
+}
+
+function renderDetailStatsInformation(hp, attack, defense, specialAttack, specialDefense, speed) {
+    return `<table>
+            <tr>
+                <th>hp:</th>
+                <td>${hp}</td>
+            </tr>
+            <tr>
+                <th>attack:</th>
+                <td>${attack}</td>
+            </tr>
+            <tr>
+                <th>defense:</th>
+                <td>${defense}</td>
+            </tr>
+            <tr>
+                <th>special-attack:</th>
+                <td>${specialAttack}</td>
+            </tr>
+             <tr>
+                <th>special-defense:</th>
+                <td>${specialDefense}</td>
+            </tr>
+            <tr>
+                <th>speed:</th>
+                <td>>${speed}</td>
+            </tr>
+        </table>
     `;
 }
